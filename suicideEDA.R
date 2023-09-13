f9dc3a38228fe7bf726b476913f970fdca408868
Paolo Speziali <speziali99@gmail.com>
2023-09-13 17:40:52 +0200

    Update suicideEDA.R

--- suicideEDA.R
+++ suicideEDA.R
@@ -3,6 +3,7 @@
 library(kableExtra)
 library(car)
 library(dplyr)
+library(reshape2)
 library(ggplot2)
 library(vioplot)
 library(sf)
@@ -38,7 +39,7 @@ length_of_Dim1 <- length(sc_data_Dim1)
 # Remove uninteresting columns
 
 to_select <-
-  c("ParentLocation","Location","Period","Dim1","FactValueNumeric","Value");
+  c("ParentLocation","Location","Period","Dim1","FactValueNumeric");
 
 sc_data_R <- sc_data[ , (names(sc_data) %in% to_select)]
 head(sc_data_R)
@@ -52,7 +53,29 @@ sc_data_R <- sc_data_R %>%
          Country = Location,
          Year = Period,
          Sex = Dim1,
-         Mean = FactValueNumeric)
+         Value = FactValueNumeric)
 head(sc_data_R)
 names(sc_data_R)
 summary(sc_data_R)
+
+# Look for null values in the feature 'Value'
+any(is.na(sc_data_R$Value))
+# No null values
+
+# Calculate no. of years for every country
+year_count <- sc_data_R %>%
+  group_by(Country) %>%
+  summarise(NumYears = n_distinct(Year))
+
+# Check if every country has got the same no. of years
+all(year_count$NumYears == max(year_count$NumYears))
+# Every country has got the same number of years
+
+# Aggregate Sex values in the same WorldRegion + Country + Year row
+sc_data_R <- dcast(sc_data_R, WorldRegion + Country + Year ~ Sex, value.var = "Value", fun.aggregate = sum)
+
+# Sort the dataset by WorldRegion + Country + Year
+sc_data_R <- sc_data_R[order(sc_data_R$WorldRegion, sc_data_R$Country, sc_data_R$Year), ]
+
+# ** Exploratory Analisis Begin **
+
